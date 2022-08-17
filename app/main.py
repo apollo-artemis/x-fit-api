da419de7b3999b4bedf6fb50df0b34a5a77d3fd7
matt <matt@second-hands.com>
2022-08-17 23:36:24 +0900

    Add create_app func in main.py

--- app/main.py
+++ app/main.py
@@ -2,24 +2,36 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from middlewares.validator import AuthRequestMiddleware
 from routers import auth, records
+import uvicorn
 
-app = FastAPI()
 
-origins = ["*"]
+def create_app():
+    app = FastAPI()
 
-# app.add_middleware(AuthRequestMiddleware)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    origins = ["*"]
 
-app.include_router(auth.router)
-app.include_router(records.router)
+    # app.add_middleware(AuthRequestMiddleware)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    app.include_router(auth.router)
+    app.include_router(records.router)
+
+    return app
+
+
+app = create_app()
 
 
 @app.get("/")
 def read_root():
     return "Hello World"
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
